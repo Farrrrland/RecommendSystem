2e010cb652b4aec20c2996f30baa38fc1da129a7
evelyn0414 <evelyn000414@gmail.com>
2021-04-11 23:16:16 +0800

    add show recommendation

--- src/components/home.vue
+++ src/components/home.vue
@@ -33,6 +33,33 @@
     <br />
     <br />
     <br />
+    <br />
+    <el-row>
+      <el-col :span="8" v-for="(o) in 2" :key="o" :offset="2">
+        <el-card :body-style="{ padding: '0px' }">
+          <img src="../assets/biscuit.png" class="image">
+          <div style="padding: 14px;">
+            <span>Afternoon tea</span><br/>
+            <span>These biscuits are really good for leisure time!</span>
+            <div class="bottom clearfix">
+              <time class="time">{{ currentDate }}</time>
+            </div>
+          </div>
+        </el-card>
+<!--        <el-card :body-style="{ padding: '0px' }">-->
+<!--          <img src="../assets/coffee.png" class="image">-->
+<!--          <div style="padding: 14px;">-->
+<!--            <span>Coffee</span>-->
+<!--            <div class="bottom clearfix">-->
+<!--              <time class="time">{{ currentDate }}</time>-->
+<!--            </div>-->
+<!--          </div>-->
+<!--        </el-card>-->
+      </el-col>
+    </el-row>
+    <br />
+    <br />
+
 <!--    <button v-on:click="add()">Add 1</button>-->
 <!--    <p>The button above has been clicked {{ counter }} times.</p>-->
 <!--    <button v-on:click="Home()"> home </button>-->
@@ -47,6 +74,36 @@
 button{
   box-shadow: inset 0 0 0 2px #212931;
 }
+.time {
+font-size: 13px;
+color: #999;
+}
+
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+}
+
+.button {
+  padding: 0;
+  float: right;
+}
+
+.image {
+  width: 100%;
+  display: block;
+}
+
+.clearfix:before,
+.clearfix:after {
+  display: table;
+  content: "";
+}
+
+.clearfix:after {
+  clear: both
+}
+
 </style>
 
 <script>
@@ -59,7 +116,8 @@ export default {
       res: {
         stat: 'failed'
       },
-      search_key:""
+      search_key:"",
+      currentDate:new Date()
     }
   },
   methods: {
